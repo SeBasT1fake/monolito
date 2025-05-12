7e5894d87af6d67d03f1007ca8a9eb48cdadc9b2
SeBasT1fake <js.acarballo2@gmail.com>
2025-05-12 15:50:43 -0500

    objetivo1

--- BookStore-monolith/app.py
+++ BookStore-monolith/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from extensions import db, login_manager
 from models.user import User
+import config  # Importa el config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookstore.db'
+app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.SQLALCHEMY_TRACK_MODIFICATIONS
 
 db.init_app(app)
 login_manager.init_app(app)
